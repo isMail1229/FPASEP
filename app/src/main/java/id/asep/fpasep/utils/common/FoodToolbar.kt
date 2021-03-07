3d91626c28b7c3f85cba33aad2221b3bd65c8c40
Asep Nurman Ismail <asep@aruna.id>
2021-03-07 19:50:25 +0700

    add extension for custom toolbar

---
+++ app/src/main/java/id/asep/fpasep/utils/common/FoodToolbar.kt
@@ -0,0 +1,39 @@
+package id.asep.fpasep.utils.common
+
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toolbar
+import id.asep.fpasep.R
+import id.asep.fpasep.utils.extension.invisible
+import id.asep.fpasep.utils.extension.setImagesToGlide
+import id.asep.fpasep.utils.extension.visible
+
+fun Toolbar.setCommonToolbar(
+    titleTv: TextView,
+    descrTv: TextView,
+    titleText: String,
+    descrText: String,
+    imageView: ImageView
+) {
+    imageView.invisible()
+    titleTv.text = titleText
+    descrTv.text = descrText
+}
+
+fun Toolbar.setToolbarWithImages(
+    titleTv: TextView,
+    descrTv: TextView,
+    titleText: String,
+    descrText: String,
+    imageView: ImageView,
+    avatarImages: String? = null
+) {
+    imageView.visible()
+    titleTv.text = titleText
+    descrTv.text = descrText
+    if (avatarImages != null) {
+        imageView.setImagesToGlide(avatarImages)
+    } else {
+        imageView.setImagesToGlide(R.drawable.ic_add_photo)
+    }
+}
