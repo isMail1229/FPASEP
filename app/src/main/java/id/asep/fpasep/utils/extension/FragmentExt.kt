36d636cb51910edfae2b15bc25ca0369620121c4
Asep Nurman Ismail <asep@aruna.id>
2021-03-07 15:21:47 +0700

    configure permission extension

--- app/src/main/java/id/asep/fpasep/utils/extension/FragmentExt.kt
+++ app/src/main/java/id/asep/fpasep/utils/extension/FragmentExt.kt
@@ -4,9 +4,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import com.google.android.material.snackbar.Snackbar
+import id.asep.fpasep.R
+import pub.devrel.easypermissions.EasyPermissions
 
 inline fun <reified VM : ViewModel> viewModels(owner: ViewModelStoreOwner) =
     ViewModelProvider(owner).get(VM::class.java)
+
+inline fun <reified VM : ViewModel> Fragment.fragmentViewModel(provider: ViewModelProvider.Factory) =
+    ViewModelProvider(this, provider).get(VM::class.java)
 
 fun <T> Fragment.observe(liveData: LiveData<T>, observer: Observer<T>) {
     liveData.observe(viewLifecycleOwner, observer)
@@ -26,4 +31,65 @@ fun Fragment.toastShort(text: String) {
 
 fun Fragment.toastLong(text: String) {
     Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
+}
+
+/*@Suppress("SpreadOperator")*/
+fun Fragment.askPermissions(
+    requestCode: Int,
+    rationaleMessage: String,
+    permissions: Array<String>,
+    afterPermissionsGrantedAction: () -> Unit
+) {
+    val ctx = context ?: return
+
+    if (EasyPermissions.hasPermissions(ctx, *permissions)) {
+        afterPermissionsGrantedAction()
+    } else {
+        EasyPermissions.requestPermissions(
+            this,
+            rationaleMessage,
+            requestCode,
+            *permissions
+        )
+    }
+}
+
+/*@Suppress("SpreadOperator")*/
+fun Fragment.handlePermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray,
+    afterPermissionsGrantedAction: () -> Unit
+) {
+    EasyPermissions.onRequestPermissionsResult(
+        requestCode,
+        permissions,
+        grantResults,
+        object : EasyPermissions.PermissionCallbacks {
+            override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+                if (EasyPermissions.somePermissionPermanentlyDenied(
+                        this@handlePermissionsResult,
+                        perms
+                    )
+                ) {
+                    Toast.makeText(
+                        context,
+                        getString(R.string.access_failed_permission_message),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onRequestPermissionsResult(
+                requestCode: Int,
+                permissions: Array<out String>,
+                grantResults: IntArray
+            ) {
+                // do nothing
+            }
+
+            override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+                afterPermissionsGrantedAction()
+            }
+        })
 }
